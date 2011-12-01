63f4b7b856f0af2fdb41528d4ef63e0e2888d546
Shan Ul Haq <gfg5.tek@gmail.com>
2011-12-01 13:50:14 +0500

    Initial Commit

---
+++ EmployeeList/EmployeeListAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EmployeeListAppDelegate.h
+//  EmployeeList
+//
+//  Created by Shan Ul Haq on 12/1/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmployeeListAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EmployeeList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EmployeeList
+//
+//  Created by Shan Ul Haq on 12/1/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EmployeeListAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EmployeeListAppDelegate class]));
+    }
+}
